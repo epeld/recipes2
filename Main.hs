3c06d52c917665a2eee8ecf96efe2736c3da2d7c
Erik Peldan <erik.peldan@gmail.com>
2017-10-27 22:43:57 +0200

    Create tables on startup

--- Main.hs
+++ Main.hs
@@ -1,4 +1,17 @@
-module Main where
 
-main :: IO ()
-main = putStrLn "Foobar"
+{-# LANGUAGE OverloadedStrings #-}
+
+import Database.MySQL.Simple
+
+connectInfo = (defaultConnectInfo { connectPassword = "abcabc", connectDatabase = "recipes" })
+
+-- setupTables :: IO ()
+setupTables conn = let q = "CREATE TABLE IF NOT EXISTS recipes (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(100) NOT NULL, PRIMARY KEY (id))"
+                   in execute_ conn q
+
+hello :: IO Int
+hello = do
+  conn <- connect connectInfo
+  setupTables conn
+  [Only i] <- query_ conn "select count(*) from recipes"
+  return i
