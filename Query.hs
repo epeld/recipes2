33590b3eeabe89dc07199191ce275272c971855f
Erik Peldan <erik.peldan@gmail.com>
2017-10-30 20:00:37 +0100

    Support querying by id

--- Query.hs
+++ Query.hs
@@ -16,15 +16,31 @@ data Options = Options
   deriving (Show, Eq)
 
 
+type ResultTuple = (Int, Text.Text, Text.Text) -- id, name, description
+
 run :: Options -> Connection -> IO ()
 run opts conn = do
-  let fmt = "%-10d%45s%45s\n"
-  xs <- query_ conn "SELECT id, name, description FROM recipes"
+  xs <- selectQuery opts conn
 
   -- Print Header
   printf "%-10s%45s%45s\n" ("RECIPE_ID" :: String) ("NAME" :: String) ("DESCRIPTION" :: String)
   putStrLn (replicate 100 '-')
   
+  let fmt = "%-10d%45s%45s\n"
   forM_ xs $ \ (id, name, description) -> do
     printf fmt (id :: Int) (Text.unpack name :: String) (take 45 (Text.unpack description) :: String)
     
+
+--selectQuery :: Options -> (Query, Params)
+selectQuery opts =
+  case Query.id opts of
+    Just i -> selectById i
+    Nothing -> selectMultipleQuery opts
+
+selectById :: RecipeId -> Connection -> IO [ResultTuple]
+selectById (RecipeId recipeId) conn = do
+  query conn "SELECT id, name, description FROM recipes WHERE id = ? LIMIT 1" (Only recipeId)
+
+selectMultipleQuery :: Options -> Connection -> IO [ResultTuple]
+selectMultipleQuery opts conn = do
+  query_ conn "SELECT id, name, description FROM recipes WHERE name LIKE 'foo' AND description LIKE 'bar' LIMIT 10"
