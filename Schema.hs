52c9aa620b037d3234fb904f09a73303d302a7c1
Erik Peldan <erik.peldan@gmail.com>
2017-11-03 21:21:20 +0100

    Add whitespace to schema

--- Schema.hs
+++ Schema.hs
@@ -11,7 +11,7 @@ data Table =
 
 
 tableCreationQuery :: Table -> Query
-tableCreationQuery Recipes = "CREATE TABLE IF NOT EXISTS recipes (id INT NOT NULL AUTO_INCREMENT,name VARCHAR(100) NOT NULL,description VARCHAR(300),PRIMARY KEY (id));"
+tableCreationQuery Recipes = "CREATE TABLE IF NOT EXISTS recipes (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(100) NOT NULL, description VARCHAR(300), PRIMARY KEY (id))"
 tableCreationQuery Ingredients = "CREATE TABLE IF NOT EXISTS ingredients (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(100) NOT NULL, PRIMARY KEY (id));"
 tableCreationQuery RecipeIngredients = "CREATE TABLE IF NOT EXISTS recipe_ingredients (id INT NOT NULL AUTO_INCREMENT, recipe_id INT NOT NULL, ingredient_id INT NOT NULL, FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE, FOREIGN KEY (ingredient_id) REFERENCES ingredients(id) ON DELETE CASCADE, PRIMARY_KEY (id));"
 
