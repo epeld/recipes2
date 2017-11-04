e5a8106a4a3b5033a45bc5f0bed7fcaa0cb91540
Erik Peldan <erik.peldan@gmail.com>
2017-11-04 10:19:22 +0100

    Add amount / unit to recipe ingredients table

--- Schema.hs
+++ Schema.hs
@@ -13,7 +13,7 @@ data Table =
 tableCreationQuery :: Table -> Query
 tableCreationQuery Recipes = "CREATE TABLE IF NOT EXISTS recipes (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(100) NOT NULL, description VARCHAR(300), PRIMARY KEY (id))"
 tableCreationQuery Ingredients = "CREATE TABLE IF NOT EXISTS ingredients (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(100) NOT NULL, PRIMARY KEY (id));"
-tableCreationQuery RecipeIngredients = "CREATE TABLE IF NOT EXISTS recipe_ingredients (id INT NOT NULL AUTO_INCREMENT, recipe_id INT NOT NULL, ingredient_id INT NOT NULL, FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE, FOREIGN KEY (ingredient_id) REFERENCES ingredients(id) ON DELETE CASCADE, PRIMARY KEY (id));"
+tableCreationQuery RecipeIngredients = "CREATE TABLE IF NOT EXISTS recipe_ingredients (id INT NOT NULL AUTO_INCREMENT, recipe_id INT NOT NULL, ingredient_id INT NOT NULL, FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE, FOREIGN KEY (ingredient_id) REFERENCES ingredients(id) ON DELETE CASCADE, amount DECIMAL NOT NULL, unit VARCHAR(50), PRIMARY KEY (id));"
 
 
 dropTableQuery :: Table -> Query
