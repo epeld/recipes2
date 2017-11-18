4884d1507dd9aca19d9a70ad2483badbb7514b28
Erik Peldan <erik.peldan@gmail.com>
2017-11-18 11:07:05 +0100

    Try using tabling for cacheing html

--- crawlers/http_service.pl
+++ crawlers/http_service.pl
@@ -3,6 +3,9 @@
 :- use_module(library(http/http_client)).
 :- use_module(stream_service, [call_with_stream/2]).
 
+:- use_module(library(tabling)).
+:- table get_html/2.
+
 user_agent('Mozilla/5.0 (X11; Linux x86_64; rv:56.0) Gecko/20100101').
 
 
