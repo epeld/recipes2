91527a2a707d92f2d3910086d93ea9f28ada3f57
Erik Peldan <erik.peldan@gmail.com>
2017-11-19 18:21:26 +0100

    Implement first version of persistence code

--- crawlers/database_service.pl
+++ crawlers/database_service.pl
@@ -5,7 +5,7 @@
 
 :- persistent
    recipe_instructions(name:text, instructions:text),
-   recipe_ingredient(recipe_name:text, ingredient_name:text, amount:number, unit:text, subgroup:text),
+   recipe_ingredient(recipe_name:text, ingredient_name:text, amount:any, unit:text, subgroup:text),
    recipe_related(name:text, related_name:text).
 
 
@@ -17,24 +17,37 @@ detach_recipe_db :-
 
 
 persist_recipe(Recipe) :-
+  persist_recipe_instructions(Recipe),
+  persist_recipe_ingredients(Recipe).
+
+
+persist_recipe_instructions(Recipe) :-
   recpe_model:recipe_name(Recipe, Name),
   recpe_model:recipe_instructions(Recipe, Instructions),
-  recpe_model:recipe_related(Recipe, Related),
-  recpe_model:recipe_ingredients(Recipe, Ingredients),
-  
-  persist_recipe_instructions(Name, Instructions),
-  forall(member(Ingredient, Ingredients),
-         persist_recipe_ingredients(Name, )).
+
+  retractall_recipe_instructions(Name, _),
+  assert_recipe_instructions(Name, Instructions).
 
 
 persist_recipe_ingredients(Recipe) :-
-  recpe_model:recipe_name(Recipe, Name),
+  recpe_model:recipe_name(Recipe, RecipeName),
   recpe_model:recipe_ingredients(Recipe, Ingredients),
+
+  with_mutex(database_service,
+             (
+               retractall_recipe_ingredient(RecipeName, _, _, _, _),
+               forall(member(Ingredient, Ingredients),
+                      persist_recipe_ingredient_model(Recipe, Ingredient))
+             )).
+
+
+persist_recipe_ingredient_model(Recipe, Ingredient) :-
+  recipe_model:recipe_name(Recipe, RecipeName),
   
-  forall(member(Ingredient, Ingredients),
-         persist_recipe_ingredient(Name, Ingredient)).
-
-
-persist_recipe_ingredient_model(RecipeName, ingredient(Name)) :-
-  assert_recipe_ingredient(RecipeName, Name, none, none, none).
+  ingredient_model:ingredient_name(Ingredient, IngredientName),
+  ingredient_model:ingredient_amount(Ingredient, Amount),
+  ingredient_model:ingredient_subgroup(Ingredient, Subgroup),
+  ingredient_model:ingredient_unit(Ingredient, Unit),
+  
+  assert_recipe_ingredient(RecipeName, IngredientName, Amount, Unit, Subgroup).
 
