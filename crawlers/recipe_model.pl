76b88b25420e580b5490843b3b34245fc211cf7f
Erik Peldan <erik.peldan@gmail.com>
2017-11-19 18:08:29 +0100

    Reimplement recipe model using library(record)

--- crawlers/recipe_model.pl
+++ crawlers/recipe_model.pl
@@ -1,33 +1,13 @@
-:- module(recipe_model, [recipe_props/2,
-                         recipe_related/2]).
+:- module(recipe_model, [make_recipe/2,
+                         default_recipe/1,
+                         recipe_name/2,
+                         recipe_instructions/2,
+                         recipe_ingredients/2,
+                         recipe_related/2
+                        ]).
 
+:- use_module(library(record)).
 
-recipe_props(Recipe, Props) :-
-  length(Props, 4),
-  member(related(_), Props),
-  member(instructions(_), Props),
-  member(name(_), Props),
-  member(ingredients(_), Props),
-  Recipe = Props. 
+:- record
+   recipe(name:text, instructions:text, ingredients:list, related:list).
 
-
-recipe_name(Recipe, Name) :-
-  recipe_prop(Recipe, name, Name).
-
-recipe_ingredients(Recipe, Ingredients) :-
-  recipe_prop(Recipe, ingredients, Ingredients).
-
-recipe_instructions(Recipe, Instructions) :-
-  recipe_prop(Recipe, instructions, Instructions).
-
-recipe_related(Recipe, Related) :-
-  recipe_prop(Recipe, related, Related).
-
-
-recipe_prop(Recipe, Name, Value) :-
-  recipe_props(Recipe, Props),
-  
-  functor(F, Name, 1),
-  arg(1, F, Value),
-  
-  member(F, Props).
